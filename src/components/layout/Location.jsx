e1cecdc4db8b52d5e0048030414579ec34313b0c
Pia Alenda <piaalendaa@gmail.com>
2025-09-04 19:56:04 -0300

    fix bugs

--- src/components/layout/Location.jsx
+++ src/components/layout/Location.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import "../../styles/components/Location.css";
 
 const Location = () => {
@@ -6,6 +6,7 @@ const Location = () => {
   const subtitleRef = useRef(null);
   const videoRef = useRef(null);
   const listRefs = useRef([]);
+  const buttonRef = useRef(null);
 
   useEffect(() => {
     const elements = [
@@ -13,6 +14,7 @@ const Location = () => {
       subtitleRef.current,
       videoRef.current,
       ...listRefs.current,
+      buttonRef.current,
     ];
 
     const observer = new IntersectionObserver(
@@ -39,24 +41,28 @@ const Location = () => {
   return (
     <section id="location" className="location-container">
       <div className="location-info">
-        <h2 ref={titleRef} className="scroll-reveal delay-0">Presencia <br />Territorial</h2>
+        <h2 ref={titleRef} className="scroll-reveal delay-0">
+          Presencia <br /> Territorial
+        </h2>
         <h3 ref={subtitleRef} className="scroll-reveal delay-0-5">1 Sede</h3>
         <ul>
-          <li ref={addToRefs} className="scroll-reveal delay-1">Escuela Obreros Por Venir</li>
+          <li ref={addToRefs} className="scroll-reveal delay-1">
+            Escuela Obreros Por Venir
+          </li>
         </ul>
 
         <a
-          href="https://www.google.com/maps/place/Escuela+T%C3%A9cnica+de+Capacitaci%C3%B3n+Laboral+N%C2%B0+917+%22Obreros+del+Porvenir%22/@-31.5412374,-68.534167,17z/data=!3m1!4b1!4m6!3m5!1s0x96814025cfa112c5:0x6a87c5233dab7f3d!8m2!3d-31.541242!4d-68.5315921!16s%2Fg%2F11fy2lrfsf?entry=ttu&g_ep=EgoyMDI1MDgyNS4wIKXMDSoASAFQAw%3D%3D"
+          href="https://www.google.com/maps/place/Escuela+T%C3%A9cnica+de+Capacitaci%C3%B3n+Laboral+N%C2%B0+917+%22Obreros+del+Porvenir%22/@-31.5412374,-68.534167,17z"
           target="_blank"
           rel="noopener noreferrer"
           className="google-map-btn scroll-reveal delay-2"
+          ref={buttonRef}
         >
-          Ver Ubicacion
+          Ver Ubicación
         </a>
       </div>
-
       <div
-        className="location-media full scroll-reveal delay-0-5"
+        className="location-media scroll-reveal delay-0-5"
         ref={videoRef}
       >
         <video
